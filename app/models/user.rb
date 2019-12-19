5ba3a6e4399930320604b5f37717bf34477e4a94
Jen <jenhoang304@gmail.com>
2019-12-19 12:14:14 -0800

    add password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    def self.digest(password)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : #conditional expression
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(password, cost: cost)
+        
+    end
+    
 end

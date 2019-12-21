c0a24f9edad550d6a551d3526f65e771332f24e2
Jen <jenhoang304@gmail.com>
2019-12-21 13:01:00 -0800

    add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :users, only:[:index, :show]
+  resources :posts
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

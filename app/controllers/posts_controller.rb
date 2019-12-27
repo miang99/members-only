bb0b90b23637d5d51c0a4078e9902bf3966c9c56
Jen <jenhoang304@gmail.com>
2019-12-27 08:50:57 -0800

    set up new action in PostController and create form for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :logged_in_user, only: [:create, :new]
     def index
         @posts = Post.all
         
@@ -10,12 +11,31 @@ class PostsController < ApplicationController
     end
 
     def create
+        @post = Post.new(strong_params)
+        if @post.save
+            flash[:success] = "Your post has been create" 
+            redirect_to root_path
+        else
+            flash.now[:warning] = "Please check your post again"
+            render "new"
+        end
+
+        
+
         
     end
 
     private
     
     def strong_params
+        params.require(:post).permit(:title, :body, :user_id)
+    end
+
+    def logged_in_user
+        if !logged_in?
+            flash[:warning] = "Please log in!!!"
+            redirect_to login_path
+        end
         
     end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: {maximum: 255}
     validates :body, presence: true
+    def author
+        user = User.find_by(id: self.user_id)
+        user.name
+        
+    end
 end
